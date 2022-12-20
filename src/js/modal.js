5590d01bb2f326600343ff60a76ddb78073fcbf9
ElchaX <maciej.sorko@gmail.com>
2022-12-20 14:08:29 +0100

    Added modal with location
    Modal is pretty basic since there is no figma for this.

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,14 +1,14 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-modal-open-contacts]'),
-    closeModalBtn: document.querySelector('[data-modal-close-contacts]'),
-    modal: document.querySelector('[data-modal-contacts]'),
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
-    refs.modal.classList.toggle('is-hidden-contacts');
+    refs.modal.classList.toggle('is-hidden');
   }
 })();
