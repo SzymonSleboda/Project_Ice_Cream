89c726db3f56e6fcf019510f45a7029b8b11dd78
SzymonSleboda <szymonskeboda@gmail.com>
2023-08-27 14:26:05 +0200

    header updated

--- src/js/modal.js
+++ src/js/modal.js
@@ -5,12 +5,20 @@
     modal: document.querySelector('[data-modal]'),
   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.openModalBtn.addEventListener('click', openModal);
+  refs.closeModalBtn.addEventListener('click', closeModal);
 
-  function toggleModal() {
+  function openModal() {
+    refs.modal.classList.add('modal__fade-in'); // Dodaj klasę animacji
+    refs.modal.classList.remove('modal__is-hidden');
+  }
 
-    refs.modal.classList.toggle('modal__is-hidden');
-
+  function closeModal() {
+    refs.modal.classList.remove('modal__fade-in'); // Usuń klasę animacji
+    refs.modal.classList.add('modal__fade-out'); // Dodaj klasę animacji
+    setTimeout(() => {
+      refs.modal.classList.remove('modal__fade-out'); // Usuń klasę animacji po zakończeniu animacji
+      refs.modal.classList.add('modal__is-hidden');
+    }, 300); // Czas trwania animacji (w milisekundach)
   }
 })();
